4433857a12186da3123e9d71cc7d5b2942a4ce7f
Andrey Bidinotto <1140669+andreymoser@users.noreply.github.com>
2025-05-15 19:32:13 -0300

    feat: countdown improvements

--- src/assets/js/countdown.js
+++ src/assets/js/countdown.js
@@ -1,20 +1,24 @@
 var countDownDate = new Date("2025-05-31T13:00:00.000Z").getTime();
-// var countDownDate = new Date().getTime() + 3600 * 1000; // 1 hour from now
+// var countDownDate = new Date().getTime() + 3600 * 1000 * 24 + 5000; // 1 day from now
+// var countDownDate = new Date().getTime() + 15000; // 15 seconds from now
 
 var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("countdown-value").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  if (distance < 0) {
+  if (distance <= 0) {
     clearInterval(x);
     document.getElementById("countdown-container").remove();
+  } else {
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var text = (days ? (days + "d ") : ("")) + 
+      (hours ? (hours + "h ") : ("")) + 
+      (minutes ? (minutes + "m ") : ("")) + 
+      seconds + "s";
+
+    document.getElementById("countdown-value").innerHTML = text;
   }
 }, 1000);
